90500f26b30df62fa2717f26c059cccd8fc097b1
M Jordan Stanway <m.j.stanway@alum.mit.edu>
2016-01-21 17:03:45 -0800

    Make serial port block, with inter-character timeout
    Removing O_NDELAY and O_NONBLOCK lets operations on the file descriptor
    (e.g., `read()`) become blocking operations.
    
    Setting `VMIN` to 255 and `VTIME` to 1 puts the file in the *(read with
    interbyte timeout)* case.
    
    (see [`termios` manpage](http://man7.org/linux/man-pages/man3/termios.3.html))
    
    This case is well-suited to bursty data (i.e., occasional packets).
    Most of our serial sensors provide data in this fashion.
    
    Essentially, with `VMIN = 255` and `VTIME = 1`, a call like:
    ```c
    ssize_t bytes_read = read(fd, buffer, nbyte);
    ```
    
    will return the first time one of the following conditions is met:
    
    * `nbyte` bytes are available
    
    * `VMIN = 255` bytes are available
    
    * `VTIME = 1` tenths of a second have passed since the last new
      byte became available.
    
    So by changing these parameters in the defaults, we are:
    
    * removing the need for `nanosleep` to prevent sensor daemons
      from spinning up extra CPU cycles to read a few characters at
      a time as they come in
    
    * removing the need for developers to specify a nominal period
      for sensors to output data
    
    at the cost of:
    
    * a *potential* 0.1 second delay
    
    To mitigate that cost:
    
    * `VMIN` could be set to the nominal data length for each sensor, or
    
    * `r2_buffer_fill` could be modified to try the nominal data length.
    
    Another potential solution would be to insert a short `nanosleep` in
    `r2_sli_stream`, *after* the `select()`, but *before* the `read()`.
    This would have the advantage of being a deterministic delay, which
    we could adjust the data timestamp to take into account.
    
    For a 9600 baud sensor with 35-character data frames, we'd want a 3.6
    millisecond delay; for a 38400 baud sensor (like the ADCP) with data
    frames of (16 + 16 + 952 + 4 = 988) bytes, we'd want a 25 millisecond
    delay.

--- r2_serial_port.h
+++ r2_serial_port.h
@@ -23,8 +23,8 @@ const struct termios R2_SERIAL_DEFAULT_OPTIONS = {
     .c_oflag = 0,
     .c_cflag = B9600 | CS8 | CLOCAL | CREAD,
     .c_lflag = 0,
-    .c_cc[VMIN] = 0,
-    .c_cc[VTIME] = 0
+    .c_cc[VMIN] = 255,
+    .c_cc[VTIME] = 1
 };
 
 /*
@@ -62,7 +62,7 @@ struct r2_serial_port * r2_serial_port_create( const char * device,
 {
     struct r2_serial_port * self = calloc( 1, sizeof( struct r2_serial_port ) );
 
-    self->fd = open( device, O_RDWR | O_NOCTTY | O_NDELAY | O_NONBLOCK );
+    self->fd = open( device, O_RDWR | O_NOCTTY );
     if( -1 == self->fd ) {
         perror( "open()" );
         fprintf( stderr, "could not open device: %s\n", device );
